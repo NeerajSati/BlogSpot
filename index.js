a45a0eef8412b388f7c916f6cc199e894bbea17e
Neeraj Sati <neeraj.sati123@gmail.com>
2021-12-04 10:55:01 +0530

    npm build

--- index.js
+++ index.js
@@ -34,10 +34,17 @@ app.use("/api/users",userRoute);
 app.use("/api/posts",postRoute);
 app.use("/api/categories",categoryRoute);
 
-app.use(express.static(path.join(__dirname,"/client/build")))
-app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname,"/client/build","index.html"))
-})
+if ( process.env.NODE_ENV == "production"){
+
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+
+    })
+
+
+}
 app.listen(PORT, () => {
     console.log('Backend hurray');
 });
