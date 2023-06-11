99bc57d58936d7183fc49c65bca4a73846fb9eee
Neeraj Sati <63944649+NeerajSati@users.noreply.github.com>
2023-06-11 19:00:05 +0530

    API url updated

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
 import axios from "axios"
 export const axiosInstance = axios.create({
-    baseURL : "https://nodeblogspot.herokuapp.com/api/"
+    baseURL : "https://node-blogspot.onrender.com/api/"
 });
