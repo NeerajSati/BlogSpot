24b62f1b9def09fdc0bec364c84c6e53c90def1e
NeerajSati <neeraj.sati123@gmail.com>
2024-03-17 16:29:57 +0530

    update styles

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,37 +1,47 @@
-import {Link} from 'react-router-dom'
-import './post.css'
-function post({post}) {
-    const origin = "https://nodeblogspot.herokuapp.com/images/";
-    const random = Math.floor((Math.random() * 25) + 10);
-    return (
-        <div className="post">
-        
-        {post.photo && (
-            <img className="postImg" src={origin + post.photo} onError={(e)=>{e.target.onerror = null; e.target.src=`https://picsum.photos/2500/16${random}`}} alt="PostImage"></img>
-        )}
-        <div className="postInfo">
-            <div className="postCats">
-                {post.categories.map((c)=>(
-                    <Link to={`/?cat=${c}`} className="link"><span className="postCat">{c}</span></Link>
-                ))}
-            </div>
-            <Link to={`/post/${post._id}`} className="link">
-            <span className="postTitle">
-                {post.title}
-            </span>
+import { Link } from "react-router-dom";
+import "./post.css";
+function post({ post }) {
+  const origin = "https://nodeblogspot.herokuapp.com/images/";
+  const random = Math.floor(Math.random() * 25 + 10);
+  return (
+    <div className="post">
+      {post.photo && (
+        <Link to={`/post/${post._id}`} className="link">
+          <img
+            className="postImg"
+            src={origin + post.photo}
+            onError={(e) => {
+              e.target.onerror = null;
+              e.target.src = `https://picsum.photos/2500/16${random}`;
+            }}
+            alt="PostImage"
+          ></img>
+        </Link>
+      )}
+      <div className="postInfo">
+        <div className="postCats">
+          {post.categories.map((c) => (
+            <Link to={`/?cat=${c}`} className="link">
+              <span className="postCat">{c}</span>
             </Link>
-            
-            <br/>
-            <Link className="link" to={`/?user=${post.username}`}>
-            <span className="postDate">
+          ))}
+        </div>
+        <Link to={`/post/${post._id}`} className="link">
+          <span className="postTitle">{post.title}</span>
+        </Link>
+
+        <br />
+        <Link className="link" to={`/?user=${post.username}`}>
+          <span className="postDate">
             Author: {post.username} | {new Date(post.createdAt).toDateString()}
-            </span>
-            </Link>
-        </div>
-        <p className="postDesc">
-        {post.desc}</p>
-        </div>
-    )
+          </span>
+        </Link>
+      </div>
+      <Link to={`/post/${post._id}`} className="link">
+        <p className="postDesc">{post.desc}</p>
+      </Link>
+    </div>
+  );
 }
 
-export default post
+export default post;
